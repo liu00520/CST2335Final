f0536683ef8d6e3d962995bf411c346767c7539b
liu00520 <liu00520@algonquinlive.com>
2021-03-05 11:12:05 -0500

    -Finished main interface -Finished click function for soccer icon Mar 5th Jia

--- app/src/main/java/com/cst2335/cst2335final/MainActivity.java
+++ app/src/main/java/com/cst2335/cst2335final/MainActivity.java
@@ -2,7 +2,9 @@ package com.cst2335.cst2335final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ******Creating OnclickListener that leads to soccer activity***************
+        ImageView soccer=  findViewById(R.id.soccer);
+        Intent goSoccer = new Intent(this, soccerActivity.class);
+        soccer.setOnClickListener( click -> startActivity(goSoccer));
+        // ******End of OnclickListener that leads to soccer activity***************
+
+
     }
 }
---
+++ app/src/main/java/com/cst2335/cst2335final/soccerActivity.java
@@ -0,0 +1,14 @@
+package com.cst2335.cst2335final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class soccerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soccer);
+    }
+}
